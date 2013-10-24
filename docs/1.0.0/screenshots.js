53eefb65576b886585099c4a9c317546e661eb13
90fcf9498f465f0090235a17a824abf584eee90b <richa.agarwal85@gmail.com>
2013-10-24 16:19:35 -0700

    Adding docs

---
+++ docs/1.0.0/screenshots.js
@@ -0,0 +1,25 @@
+[source,js]
+----
+var casper = require('casper').create({
+    viewportSize : {width : 800, height : 600},
+    verbose: true,
+    logLevel: "debug",
+});
+
+casper.start("http://localhost:5000", function userInterface() {
+    this.capture("index.png");
+});
+
+casper.thenOpen('http://localhost:5000/request/1', function userInterface() {
+    this.captureSelector("request1response.png", "#responses-container");
+    this.captureSelector("request1request.png", "#request-container");
+ });
+
+casper.thenOpen('http://records.oaklandnet.com/api/request/1', function userInterface() {
+	var fs = require('fs');
+    fs.write('api.json', this.getPageContent());
+ });
+
+
+casper.run();
+----
